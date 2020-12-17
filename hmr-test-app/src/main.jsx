85f74605165788a7a66569f6eb7876d23264e486
remorses <beats.by.morse@gmail.com>
2020-12-17 13:37:19 +0100

    import.meta.hot.accept() in snowpack

--- hmr-test-app/src/main.jsx
+++ hmr-test-app/src/main.jsx
@@ -3,3 +3,7 @@ import { App } from './bridge'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+if (import.meta.hot) {
+    import.meta.hot.accept()
+}
